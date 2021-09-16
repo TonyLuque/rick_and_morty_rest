3ec1859a9039916b0a660b23112d1498d13f3ea0
anthony luque <anthony.luque@evo-ion.com>
2021-09-16 12:35:43 -0500

    [ADD] styles to testdescription and fix styles button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 export const _Container = styled.div`
   background-color: #11555f;
   border-radius: 60px;
-  padding: 0 20px;
+  padding: 11px 20px;
   cursor: pointer;
 `;
 
 export const _Text = styled.p`
   font-size: 18px;
+  margin: 0px;
 `;
