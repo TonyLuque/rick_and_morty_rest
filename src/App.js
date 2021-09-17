2cead14948ee3ee105342cb9afd1d2025cfa72d8
Anthony Luque <kasuru@hotmail.com>
2021-09-17 10:03:24 -0500

    [FIX} routes blank page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 import Characters from "./screens/Characters";
 import Inicio from "./screens/Inicio";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Inicio} />
         <Route exact path="/characters" component={Characters} />
+        <Redirect to="/characters" />
         {/* {/* <Route exact path="/signup" component={Signup} /> */}
       </Switch>
-    </div>
+    </BrowserRouter>
   );
 }
 
